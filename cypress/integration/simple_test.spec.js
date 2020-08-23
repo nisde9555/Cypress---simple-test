2b3db5d47ac6257d3b9883fd10c358975f378287
denis <bergkamp777@gmail.com>
2020-08-23 19:07:36 +0200

    Add utils 	modified:   cypress/integration/simple_test.spec.js 	new file:   cypress/utils/utils.js

--- cypress/integration/simple_test.spec.js
+++ cypress/integration/simple_test.spec.js
@@ -1,23 +1,24 @@
 import {googleSearch, imdbDomain} from '../variables/variables';
+import {getAriaLabel} from '../utils/utils';
 
 describe('Simple test', () => {
   it('Search on google', () => {
     cy.visit('https://www.google.com/')
     cy.get('.RNNXgb').then($body => {
       if ($body.get('input[aria-label="Search"]')) {
-        cy.get('input[aria-label="Search"]')
-        .type(googleSearch)
-        .should('have.value', googleSearch)
-        cy.get('body').trigger('keydown', { keyCode: 27, which: 27 });
-        cy.get('input[aria-label="Google Search"]').click({ multiple: true, force: true })
-      } else {
-        cy.get('input[aria-label="Тражи"]')
+        getAriaLabel("input", "Search")
           .type(googleSearch)
           .should('have.value', googleSearch)
         cy.get('body').trigger('keydown', { keyCode: 27, which: 27 });
-        cy.get('input[aria-label="Google претрага"]').click({ multiple: true, force: true })
+        getAriaLabel("input", "Google Search").click({ multiple: true, force: true })
+      } else {
+        getAriaLabel("input", "Тражи")
+          .type(googleSearch)
+          .should('have.value', googleSearch)
+        cy.get('body').trigger('keydown', { keyCode: 27, which: 27 });
+        getAriaLabel("input", "Google претрага").click({ multiple: true, force: true })
       }
-    cy.contains(imdbDomain)
+    cy.contains(imdbDomain).should('be.visible');
     })
   })
 })
